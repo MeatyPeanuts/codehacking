3e291024788e80a60424ae1d12f86cf660140973
Mike <miked@wecreate.com>
2019-04-09 09:55:23 -0400

    Persist users into database without pictures

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -49,7 +49,10 @@ class AdminUsersController extends Controller
     {
         //
 
-        return $request->all();
+        User::create($request->all());
+
+        return redirect('/admin/users');
+//        return $request->all();
 
     }
 
